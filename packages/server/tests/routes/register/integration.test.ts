6bbe342a43f5414b1a62002d087f8479e3180edf
Dibash Thapa <dibashthapa55@gmail.com>
2021-12-21 00:05:27 +0545

    feat(tests): add register tests

--- packages/server/tests/routes/register/integration.test.ts
+++ packages/server/tests/routes/register/integration.test.ts
@@ -6,15 +6,13 @@ import { User } from "@prisma/client";
 import { createHash } from "../../../src/utils/hash";
 import { torrentQueue } from "../../../src/queues/torrentQueue";
 
-describe("Login basic route", () => {
-  const endpoint = "/login/basic";
+describe("Register basic route", () => {
+  const endpoint = "/register/basic";
   const request = supertest(app);
-
   let user: User;
 
   beforeAll(async () => {
     const hashedPassword = await createHash("password");
-
     user = await prismaClient.user.create({
       data: {
         name: "dibash",
@@ -35,21 +33,16 @@ describe("Login basic route", () => {
     expect(response.status).toBe(400);
   });
 
-  it("should send success when correct credentials is provided", async () => {
+  it("should send error when user already exists", async () => {
     const response = await addHeaders(
-      request.post(endpoint).send({ email: user.email, password: "password" })
+      request
+        .post(endpoint)
+        .send({ email: user.email, password: "password", name: "hello" })
     );
-    expect(response.status).toBe(200);
-  });
-
-  it("should send error when only email is sent", async () => {
-    const response = await addHeaders(
-      request.post(endpoint).send({ email: user.email })
-    );
+    expect(response.text).toMatch(/already exists/);
     expect(response.status).toBe(400);
   });
 });
-
- const addHeaders = (request: any) => {
+const addHeaders = (request: any) => {
   return request.set("Content-Type", "application/json");
 };
